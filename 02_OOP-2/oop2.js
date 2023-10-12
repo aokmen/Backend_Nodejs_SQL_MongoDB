413a59d00e9b24e47134614fcd53ba781ffd5179
aokmen <abdullah.okmen.11@gmail.com>
2023-10-12 16:22:33 +0200

    Encapsulation_examples_made

--- 02_OOP-2/oop2.js
+++ 02_OOP-2/oop2.js
@@ -71,3 +71,53 @@ car1.brand='ford'
 console.log(car1.brand);
 const car2 = new Car('nissan','micra',2020)
 console.log(car2);
+
+/* ----------------------------------- ... ---------------------------------- */
+class Enemy {
+    constructor(power) {
+        this.power = power
+    }
+
+    attack = () => console.log(`I'm attacking with a power of ${this.power}!`)
+}
+
+class Alien extends Enemy {
+    constructor(name,phrase,power){
+        super(power)
+        this.name=name
+        this.phrase=phrase
+        this.species="alien"
+
+    }
+    fly = () => console.log("Hi");
+    sayPhrase = () => console.log(this.phrase);
+}
+const alien1 = new Alien("Ali","Hello",10)
+
+console.log(alien1);
+console.log(alien1.attack(10));
+
+/* -------------------------------------------------------------------------- */
+/*                                Encapsulation                               */
+/* -------------------------------------------------------------------------- */
+
+
+// Here's our class
+class Alien2 extends Alien {
+     #birthYear // We first need to declare the private property, always using the '#' symbol as the start of its name.
+
+    constructor (name, phrase, power, speed, birthYear) {
+        super(name, phrase, power, speed)
+        this.speed = speed
+        this.#birthYear = birthYear // Then we assign its value within the constructor function
+    }
+    fly = () => console.log("Zzzzzziiiiiinnnnnggggg!!")
+    howOld = () => console.log(`I was born in ${this.#birthYear}`) // and use it in the corresponding method.
+}
+    
+// We instantiate the same way we always do
+const alien2 = new Alien2("Ali", "I'm Ali the alien!", 10, 50, 10000)
+
+console.log(alien2);
+alien2.howOld()
+
