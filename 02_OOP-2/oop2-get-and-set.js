526a5698c04bee7e24b429fb0f910753f37d49bc
aokmen <abdullah.okmen.11@gmail.com>
2023-10-13 08:43:10 +0200

    OOP_getter_setter_created

---
+++ 02_OOP-2/oop2-get-and-set.js
@@ -0,0 +1,26 @@
+'use strict'
+//? GETTER (veri getirme) and SETTER (değer atama /güncelleme) methods
+
+class Car{
+    #price
+    constructor(marka,model){
+        this.marka=marka
+        this.model=model
+    }
+    // getPrice(){
+    //     return this.#price
+    // } 
+    get getPrice(){
+        return this.#price
+    } 
+    set setPrice(prc){
+        this.#price=prc
+    } 
+
+}
+const car1=new Car('mrcedes','benz')
+console.log(car1)
+// car1.setPrice(2000)
+car1.setPrice=2000 // seter yöntemi ile yazıldı ise
+console.log(car1)
+console.log(car1.getPrice)
