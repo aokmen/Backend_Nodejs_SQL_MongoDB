7da8d6e709b5395332822728789d68e95fe7ed42
Abdullah <129761775+aokmen@users.noreply.github.com>
2023-10-16 15:48:05 +0200

    Update SQL-DB.js

--- SQL-DB.js
+++ SQL-DB.js
@@ -1,27 +1,27 @@
 
 SELECT * FROM employees;
-// Belirli bir departmanda çalışanları seçme:
+// Select employees in a specific department:
 
 SELECT * FROM employees WHERE department = 'Sales';
-// Belirli bir maaş aralığındaki çalışanları seçme:
+// Select employees in a specific salary range:
 
 
 SELECT * FROM employees WHERE salary BETWEEN 30000 AND 50000;
-// Belirli bir tarihten sonraki siparişleri seçme:
+// Selecting orders after a specific date:
 
 SELECT * FROM orders WHERE order_date > '2023-01-01';
-// Belirli bir müşterinin siparişlerini seçme:
+// Selecting orders from a specific customer:
 
 
 SELECT * FROM orders WHERE customer_id = 12345;
-// Belirli bir şehirdeki müşterileri seçme:
+// Selecting customers from a specific city:
 
 
 SELECT * FROM customers WHERE city = 'New York';
-// Stokta sadece belirli bir miktarın üzerinde ürünleri seçme:
+// Selecting only products above a certain quantity in stock:
 
 
 SELECT * FROM products WHERE stock_quantity > 50;
-// Belirli bir kategoriye ait ürünleri seçme:
+// Select products belonging to a specific category:
 
 SELECT * FROM products WHERE category = 'Electronics';
