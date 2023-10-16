240e171d5bda05f92578d15eba6dc4a93f555dab
Abdullah <129761775+aokmen@users.noreply.github.com>
2023-10-16 15:48:35 +0200

    Update SQL-DB.js

--- SQL-DB.js
+++ SQL-DB.js
@@ -5,21 +5,17 @@ SELECT * FROM employees;
 SELECT * FROM employees WHERE department = 'Sales';
 // Select employees in a specific salary range:
 
-
 SELECT * FROM employees WHERE salary BETWEEN 30000 AND 50000;
 // Selecting orders after a specific date:
 
 SELECT * FROM orders WHERE order_date > '2023-01-01';
 // Selecting orders from a specific customer:
 
-
 SELECT * FROM orders WHERE customer_id = 12345;
 // Selecting customers from a specific city:
 
-
 SELECT * FROM customers WHERE city = 'New York';
 // Selecting only products above a certain quantity in stock:
-
 
 SELECT * FROM products WHERE stock_quantity > 50;
 // Select products belonging to a specific category:
