41e4303b671c5d880cb87c71b6f4f8444ed3a7dc
aokmen <abdullah.okmen.11@gmail.com>
2023-10-25 11:18:48 +0200

    mongosh_Collections_dates_created

--- 06_Mongo_DB/02_Mongo.js
+++ 06_Mongo_DB/02_Mongo.js
@@ -1,22 +1,108 @@
+
+/* - 1. Kaç tür SQL dili vardır ve hangisi hangi tür komutları çalıştırmak içindir? - */
+
+
+// SQL Yapılandırılmış Sorgu Dili "Structured Query Language", farklı komutları çalıştırmak için başta olmak üzere çeşitli tiplerde gelir:
+// Veri Sorgu Dili "Data Query Language" (DQL): Veritabanlarından veri sorgulamak için kullanılır. DQL'deki ana komut SELECT'tir.
+// Veri Tanımlama Dili "Data Definition Language" (DDL): Veritabanı yapılarını tanımlamak ve yönetmek için kullanılır. CREATE, ALTER ve DROP gibi komutlar DDL'ye dahildir.
+// Veri İşleme Dili "Data Manipulation Language" (DML): Veritabanındaki verileri yönetmek için kullanılır. INSERT, UPDATE ve DELETE gibi komutlar DML'nin bir parçasıdır.
+// Veri Kontrol Dili "Data Control Language" (DCL): Veritabanında erişimi ve izinleri kontrol etmek için kullanılır. GRANT ve REVOKE gibi komutlar DCL'nin bir parçasıdır.
+
+
+/* ----------------------- 2. NOSQL'in açılımı nedir? ----------------------- */
+
+
+// NoSQL, "Sadece SQL Değil" anlamına gelir. Bu, geleneksel ilişkisel veritabanı yönetim sistemine (RDBMS) dayanmayan bir veritabanı yönetim sistemlerinin türüdür. NoSQL veritabanları, yapısal olmayan veya yarı yapısal verileri işlemek üzere tasarlanmıştır ve çeşitli veri depolama ve alım ihtiyaçları için uygundur.
+
+
+/* ----- 3. Express.js neden mongoDB kullanıyor, nedenleri ne olabilir? ----- */
+
+
+// Express.js, popüler bir Node.js web uygulama çerçevesi, çeşitli nedenlerle MongoDB'i kullanır:
+
+// JSON Uyumluluğu: MongoDB'in belge odaklı doğası ve BSON (Binary JSON) desteği, JavaScript ve Express.js ile doğal bir uyum içindedir, çünkü bu teknolojiler JSON verileriyle iyi çalışırlar.
+
+// Ölçeklenebilirlik: MongoDB, artan veri yüklerini ve trafiği işlemek için yatay olarak kolayca ölçeklenebilen bir NoSQL veritabanıdır, bu da Express.js ile oluşturulan web uygulamaları için uygundur.
+
+// Esneklik: MongoDB'in şema eksikliği, geliştiricilere uygulamanın evrildikçe veri yapısını kolayca adapte etmelerine olanak tanır, bu da hızlı geliştirme döngüsüne sahip web uygulamalarında faydalıdır.
+
+// Hız: MongoDB, yüksek okuma ve yazma performansıyla bilinir, bu da verilerin hızlı bir şekilde alınmasının ve depolanmasının önemli olduğu web uygulamaları için önemlidir.
+
+// Topluluk ve Ekosistem: MongoDB, güçlü bir topluluğa ve araçlarla kütüphanelerin zengin bir ekosistemine sahiptir, bu da Express.js kullanan web geliştiricileri için popüler bir tercih haline getirir.
+
+
+
+/* -------- 5. MVC yapısını aşağıdaki hikayeyle eşleştirebilir misiniz? -------- */
+
+
+// Hangisi denetleyici, hangisi model, hangisi görünüm?
+// Bir restorana gittiğinizi varsayalım. Her ne kadar bunu yapabiliyor olsanız bile, mutfakta yemek pişirmeyeceksiniz.
+// Ev. Bunun yerine işletmeyi ziyaret edip garsonun gelmesini beklersiniz.
+// Garson şimdi yanınıza yaklaşıyor ve siz de akşam yemeği siparişinizi veriyorsunuz. Sunucu yalnızca şunu yazdı:
+// yemek siparişinizin ayrıntıları; sizin kim olduğunuzun ve ne arzuladığınızın farkında değil.
+// Garson daha sonra mutfağa doğru ilerliyor. Sunucu yemeğinizi mutfakta hazırlamıyor.
+// Yemeğiniz aşçı tarafından hazırlanır. Siparişiniz ve masa numaranız garsona verilmektedir.
+// Yemek yap, sonra senin için yemek hazırla. Yemek kendisi tarafından malzemeler kullanılarak hazırlanır. diyelim ki sen
+// sebzeli sandviç seçeneğini seçin. Daha sonra ekmeği, domatesi, patatesi almak için buzdolabını kullanıyor.
+// ihtiyacı olan kırmızı biber, soğan, biraz ve peynir.
+// Şef, yemeğin son parçasını garsona verir. Bu yemeği mutfağın dışına taşımak artık garsonun işi
+// sorumluluk.
+// Sunucu artık sipariş ettiğiniz yiyeceklerin ve bunların nasıl servis edileceğinin farkındadır.
+
+// Model: Hikayede "aşçı," Model'i temsil eder. Aşçı, yemeği hazırlamaktan sorumludur, bu da bir uygulamadaki veri ve iş mantığına karşılık gelir. Model, veri depolama, alımı ve işlemeyi ele alır.
+
+// View: "Garson," Görünüm ile ilişkilendirilebilir. Garson, müşteriden siparişi alır, aracı rol oynar ve yemeği müşteriye servis eder. Benzer şekilde, MVC mimarisinde Görünüm, veriyi kullanıcıya sunma ve kullanıcı etkileşimlerini ele alma sorumluluğundadır.
+
+// Controller: Bu benzetimde "müşteri," Denetleyici'yi temsil edebilir. Müşteri, siparişi vererek etkileşimi başlatır, bu da kullanıcı girişi ve eylemlerle uyumlu bir rol oynar. Müşteri yemeği doğrudan hazırlamaz (aşçı gibi) veya servis etmez (garson gibi) ancak neler olacağını belirlemede rol oynar ve etkileşimin akışını kontrol eder.
+
+// Bu nedenle, MVC yapısında:
+
+// Model (Aşçı), veri ve iş mantığını yönetir.
+// View (Garson), veriyi kullanıcıya sunar ve kullanıcı etkileşimlerini ele alır.
+// Controller (Müşteri), eylemleri başlatır ve etkileşimin akışını kontrol eder.
+
+
+ /* ---------- 4. Aşağıdaki tablolardaki verileri MongoDb'de olustur --------- */
+
+// Table name "post"
+
+// post_id    title
+// 1          amazing duo Express.js and MongoDb
+// 2          MVC architecture
+
+// Table name "comment"
+
+// comment_id post_id author content
+// 1 1 Qadir I agree with you
+// 2 1 Victor Their communities are very good
+// 3 2 Rafe Django use MVT
+// 4 1 Cedric Good post
+
+
+
+// mongosh
 use("tw") // create database
 show("dbs") // show databases
 db.createCollection("posts") // create table named "posts"
+
 db.posts.insertMany([{ "başlık": "İlk Gönderi", "içerik": "Bu, bir...", "oluşturulma_tarihi": new Date("2023-10-21T09:00:00Z") }, { "başlık": "İkinci Gönderi", "içerik": "Başka bir...", "oluşturulma_tarihi": new Date("2023-10-21T10:30:00Z") }, { "başlık": "Üçüncü Gönderi", "içerik": "Bir başka...", "oluşturulma_tarihi": new Date("2023-10-21T14:45:00Z") }])
-db.posts.find()  // show table
-+----+------------+-------------------+---------------------+
-| ID | Başlık     | İçerik            | Oluşturulma Tarihi |
-+----+------------+-------------------+---------------------+
-| 1  | İlk Gönderi| Bu, bir...        | 2023-10-21 09:00:00 |
-| 2  | İkinci Gönderi| Başka bir...    | 2023-10-21 10:30:00 |
-| 3  | Üçüncü Gönderi| Bir başka...    | 2023-10-21 14:45:00 |
-+----+------------+-------------------+---------------------+
+db.posts.find()  //
+
+// +----+------------+-------------------+---------------------+
+// | ID | Başlık     | İçerik            | Oluşturulma Tarihi  |
+// +----+------------+-------------------+---------------------+
+// | 1  | 1. Gönderi | Bu, bir...        | 2023-10-21 09:00:00 |
+// | 2  | 2. Gönderi | Başka bir...      | 2023-10-21 10:30:00 |
+// | 3  | 3. Gönderi | Bir başka...      | 2023-10-21 14:45:00 |
+// +----+------------+-------------------+---------------------+
+
 db.posts.drop()  // remove table
 
  /* ----------------------------------- --- ---------------------------------- */
 
 db.createCollection("post")
 db.post.insertMany([{ "post_id": 1, "title": "amazing duo Express.js and MongoDb" },{ "post_id": 2, "title": "MVC architecture" }])
-db.comments.insertMany([{ "comment_id": 1, "post_id": 1, "author": "Qadir", "content": "I agree with you" },{ "comment_id": 2, "post_id": 1, "author": "Victor", "content": "Their communities are very good" },{ "comment_id": 3, "post_id": 2, "author": "Rafe", "content": "Django uses MVT" },{ "comment_id": 4, "post_id": 1, "author": "Cedric", "content": "Good post" }])
+db.comment.insertMany([{ "comment_id": 1, "post_id": 1, "author": "Qadir", "content": "I agree with you" },{ "comment_id": 2, "post_id": 1, "author": "Victor", "content": "Their communities are very good" },{ "comment_id": 3, "post_id": 2, "author": "Rafe", "content": "Django uses MVT" },{ "comment_id": 4, "post_id": 1, "author": "Cedric", "content": "Good post" }])
 
 /* Controller: The waiter in the restaurant serves as the controller. The controller takes your food order and communicates it to the kitchen (model). The controller handles the interaction between the user (you) and the system (kitchen and chef).
 
@@ -69,7 +155,7 @@ db.people.find({}, { "user_id": 1, "status": 1, "_id": 0 })
 /*      4.MongoDB equivalent of SELECT * FROM people WHERE status = "A":      */
 /* -------------------------------------------------------------------------- */
  
- 
+ ,{id:1,user_id:1,age:1,status:1,_id:0}
 db.people.find({ "status": "A" })
 
  /* ---------------------------------------------------------------------- */
@@ -82,7 +168,7 @@ db.people.find({ "status": "A" }, { "user_id": 1, "status": 1, "_id": 0 })
 /*      6.MongoDB equivalent of SELECT * FROM people WHERE status != "A":     */
 /* -------------------------------------------------------------------------- */
  
-db.people.find({ "status": { $ne: "A" } })
+db.people.find({ "status": { $ne: "A" } }) // not equal
 
 /* -------------------------------------------------------------------------- */
 /* 7.MongoDB eq of SELECT * FROM people WHERE status  = "A" AND age = 50:     */
@@ -100,13 +186,13 @@ db.people.find({ $or: [{ "status": "A" }, { "age": 50 }] })
 /*        9.MongoDB equivalent of SELECT * FROM people WHERE age > 25:        */
 /* -------------------------------------------------------------------------- */
  
-db.people.find({ "age": { $gt: 25 } })
+db.people.find({ "age": { $gt: 25 } }) // greater-than
 
 /* -------------------------------------------------------------------------- */
 /*        10.MongoDB equivalent of SELECT * FROM people WHERE age < 25:       */
 /* -------------------------------------------------------------------------- */
  
-db.people.find({ "age": { $lt: 25 } })
+db.people.find({ "age": { $lt: 25 } }) //  less-than
 
 /* -------------------------------------------------------------------------- */
 /*     11.MongoDB eq of SELECT * FROM people WHERE age > 25 AND age <= 50:    */
@@ -171,9 +257,56 @@ db.people.find({}).skip(10).limit(5)
 /* -------------------------------------------------------------------------- */
 /*   21. MDB collection "people" with a similar structure to the SQL table:   */
 /* -------------------------------------------------------------------------- */
- 
+// CREATE TABLE people (
+//   id MEDIUMINT NOT NULL
+//   AUTO_INCREMENT,
+//   user_id Varchar(30),
+//   age Number,
+//   status char(1),
+//   P 
+
+
 db.createCollection("people")
 db.people.createIndex({ "id": 1 }, { unique: true })
+
+db.people.insertOne({
+   "user_id": "YourUserID",
+   "age": 30,
+   "status": "A"
+})
+
+
+
+// { id: 1, user_id: 'user1', age: 25, status: 'A' },
+// { id: 2, user_id: 'user2', age: 30, status: 'B' },
+// { id: 3, user_id: 'user3', age: 35, status: 'A' },
+// { id: 4, user_id: 'user4', age: 40, status: 'C' },
+// { id: 5, user_id: 'user5', age: 45, status: 'A' }
+
+
+// db.people.createIndex({ "id": 1 }, { unique: true }) ifadesiyle oluşturulan benzersiz indeks, "id" alanındaki değerlerin yinelenmeden yalnızca bir kez kullanılmasını zorlar. Örneğin, koleksiyonda yalnızca bir "id: 1" olabilir, yani aynı "id" değerine sahip iki belge eklenemez.
+
+// Bu nedenle, "id: 1" değeriyle bir belge ekledikten sonra başka bir belgeyi "id: 1" değeriyle eklemeye çalışırsanız, MongoDB bir benzersizlik hatası verecektir. Ancak, "id: 2", "id: 3" gibi diğer farklı "id" değerleri kullanılabilir ve bu belirli değerlere sahip belgeler koleksiyonda birer kez bulunabilir.
+
+// Diyelim ki db.people.createIndex({ "id": 1 }, { unique: true }) ifadesiyle oluşturulan indeksi kullanarak aşağıdaki işlemleri gerçekleştiriyorsunuz:
+
+// İlk belgeyi eklediniz: { id: 1, user_id: 'user1', age: 25, status: 'A' }
+// Bu işlem başarılı olur çünkü koleksiyonda henüz "id: 1" değerine sahip başka bir belge yok.
+
+// İkinci belgeyi eklemeye çalıştınız: { id: 1, user_id: 'user2', age: 30, status: 'B' }
+// Bu işlem benzersizlik kuralına aykırıdır ve MongoDB bu belgeyi eklerken bir hata fırlatır. Çünkü "id: 1" zaten koleksiyonda bulunan bir belgeye aittir ve aynı "id" değeri tekrar kullanılamaz.
+
+// Üçüncü belgeyi eklersiniz: { id: 2, user_id: 'user3', age: 35, status: 'A' }
+// Bu işlem başarılıdır çünkü "id: 2" değeri daha önce kullanılmamıştır ve indeks, yeni bir "id" değerine izin verir.
+
+// Bu nedenle, bu indeksi kullanarak MongoDB koleksiyonunda "id" alanını benzersiz bir anahtar olarak kullanabilirsiniz. Her belge "id" alanıyla bir kez temsil edilir, ve aynı "id" değerine sahip başka bir belge eklemeye çalışırsanız MongoDB bu işlemi reddeder ve benzersizlik ihlali hatası verir.
+
+
+// db.people.createIndex({ "id": 1 }, { unique: true })
+// db.people.insertOne({id: 1, user_id: 'user1', age: 25, status: 'A'})
+
+
+
 
  /* -------------------------------------------------------------------------- */
  /*                22.ALTER TABLE people ADD join_date DATETIME                */
