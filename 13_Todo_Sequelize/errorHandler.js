715aab07de7ef27285e009bf4a2f49fe99a83a84
Abdullah <129761775+aokmen@users.noreply.github.com>
2023-10-31 16:47:58 +0100

    Update errorHandler.js (app.all ,req,res)

--- 13_Todo_Sequelize/errorHandler.js
+++ 13_Todo_Sequelize/errorHandler.js
@@ -7,8 +7,8 @@ const HOST = process.env.HOST || '127.0.0.1'
 
 app.use(express.json())  // bu ifade ile json formatinda veri gönderilebiliyor
 
-// app.all('/',(req,res)=>{
-//     res.send('welcome')
-// })
+ app.all('/',(req,res)=>{
+    res.send('welcome')
+ })
 
 app.use(require('./todoRouter'))
