0a3d36528bb620e346c715f62b197d13aa726b94
aokmen <abdullah.okmen.11@gmail.com>
2023-11-02 13:07:38 +0100

    User and blog Routes added to BlogApi

--- 14_BlogAPI-Mongoose/src/routes/blogRoutes.js
+++ 14_BlogAPI-Mongoose/src/routes/blogRoutes.js
@@ -1,14 +1,27 @@
-/* -------------------------------------------------------------------------- */
-/*                                 Blog Routes                                */
-/* -------------------------------------------------------------------------- */
-
+/*   BLOG ROUTES        */
 'use strict'
-const router = require ('express').Router()
 
-const {BlogPost} = require('../controller/blogController')
+const router=require('express').Router()
 
-router.route('/')
+const {BlogPost,BlogCategory}=require('../controllers/blogController')
+
+router.route('/category')
+    .get(BlogCategory.list)
+    .post(BlogCategory.create)
+
+router.route('/category/:categoryId')
+    .get(BlogCategory.read)
+    .put(BlogCategory.update)
+    .delete(BlogCategory.delete)
+
+
+router.route('/post')
     .get(BlogPost.list)
     .post(BlogPost.create)
 
-module.exports = router
+router.route('/post/:postId')
+    .get(BlogPost.read)
+    .put(BlogPost.update)
+    .delete(BlogPost.delete)
+
+module.exports=router
