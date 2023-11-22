685bdce6b0c11cc9173163aa74677b79765a7c4d
aokmen <abdullah.okmen.11@gmail.com>
2023-11-22 20:45:44 +0100

    User and reservation models were created .

---
+++ 22_FlightAPI-2/src/routes/reservation.js
@@ -0,0 +1,26 @@
+"use strict"
+/* -------------------------------------------------------
+    NODEJS EXPRESS | CLARUSWAY FullStack Team
+------------------------------------------------------- */
+const router = require('express').Router()
+/* ------------------------------------------------------- */
+// routes/user:
+
+const permissions = require('../middlewares/permissions')
+const reservation = require('../controllers/reservation')
+
+// URL: /reservations
+router.use(permissions.isStaff)
+
+router.route('/')
+    .get(reservation.list)
+    .post(reservation.create)
+
+router.route('/:id')
+    .get( reservation.read)
+    .put( reservation.update)
+    .patch( reservation.update)
+    .delete( reservation.delete)
+
+/* ------------------------------------------------------- */
+module.exports = router
