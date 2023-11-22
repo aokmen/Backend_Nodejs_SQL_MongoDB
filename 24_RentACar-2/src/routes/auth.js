490e5feeda7e22ed526da5cbb028de3b7e142e35
aokmen <abdullah.okmen.11@gmail.com>
2023-11-22 20:43:57 +0100

    Token removed in logout processes

---
+++ 24_RentACar-2/src/routes/auth.js
@@ -0,0 +1,18 @@
+"use strict"
+/* -------------------------------------------------------
+    NODEJS EXPRESS | CLARUSWAY FullStack Team
+------------------------------------------------------- */
+const router = require('express').Router()
+/* ------------------------------------------------------- */
+// routes/auth:
+
+const auth = require('../controllers/auth')
+
+// URL: /auth
+
+router.post('/login', auth.login)
+router.get('/logout', auth.logout)
+router.all('/logout', auth.logout)
+
+/* ------------------------------------------------------- */
+module.exports = router
