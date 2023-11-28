f30cd8f0a3255a8595090671a7ca7bae08e0d3b8
aokmen <abdullah.okmen.11@gmail.com>
2023-11-28 20:44:13 +0100

    with purchase_Id changed product_total

--- 26_Stock-1/src/routes/purchase.js
+++ 26_Stock-1/src/routes/purchase.js
@@ -4,3 +4,16 @@
 ------------------------------------------------------- */
 const router = require('express').Router()
 /* ------------------------------------------------------- */
+const purchase = require('../controllers/purchase')
+
+router.route('/')
+    .get(purchase.list) 
+    .post(purchase.create)
+
+router.route('/:id')
+    .get(purchase.read) 
+    .put(purchase.update)
+    .patch(purchase.update)
+    .delete(purchase.delete)
+
+module.exports=router
