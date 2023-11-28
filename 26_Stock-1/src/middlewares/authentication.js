90a23c21e04058a7a5c2a07fe36bcd5fc53f79ec
aokmen <abdullah.okmen.11@gmail.com>
2023-11-28 20:47:17 +0100

    User authenticated by token

--- 26_Stock-1/src/middlewares/authentication.js
+++ 26_Stock-1/src/middlewares/authentication.js
@@ -3,8 +3,20 @@
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
 // app.use(authentication):
+const Token=require('..//models/token')
 
 module.exports = async (req, res, next) => {
+
+    const auth=req.headers?.authorization || null
+    const tokenKey=auth ? auth.split(' '):null
+        
+        if(tokenKey && tokenKey[0]=='Token'){
+            const tokenData = await Token.findOne({token: tokenKey[1]})
+            req.user=tokenData ? tokenData.user_id : undefined
+            // console.log('************');
+            // console.log(req.user);
+        }
+
 
     next()
 }
