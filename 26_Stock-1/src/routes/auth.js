f7e66304809b3deb1b7fda069dc30b2fb75e0cf5
aokmen <abdullah.okmen.11@gmail.com>
2023-11-28 20:44:54 +0100

    login and logout controlled with token

--- 26_Stock-1/src/routes/auth.js
+++ 26_Stock-1/src/routes/auth.js
@@ -4,3 +4,10 @@
 ------------------------------------------------------- */
 const router = require('express').Router()
 /* ------------------------------------------------------- */
+const auth = require('../controllers/auth')
+
+router.post('/login',auth.login) 
+router.get('/logout',auth.logout)
+
+
+module.exports=router
